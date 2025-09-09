2e6885ad47f468f3e6c8d48c897c4a84b3100ec1
Adrian NOYES <93002614+Pingasmaster@users.noreply.github.com>
2025-09-09 16:39:02 +0200

    Create main.ts
    Première version du main.ts
    
    Signed-off-by: Adrian NOYES <93002614+Pingasmaster@users.noreply.github.com>

---
+++ main.ts
@@ -0,0 +1,22 @@
+import { loadAssets, simulateDownload } from 'utils/assets.js',
+import { Game } from './core/game.js'
+
+async function bootstrap() {
+  const canvas = document.getElementById('game') as HTMLCanvasElement;
+  const cx =  canvas.getContext('2d');
+  if (ctx) throw new Error('Canvas 2D non supporté, essayez de voir si votre navigateur est à jour et javascript est activé');
+
+  // Promesse then avec erreur catch
+  simulateDownload('atlas').then(msg => console.log(msg)).catch(err => console.warn('Erreur du simulateur, ça devrait pas arriver mais bon:', err.message));
+  try {
+    const assets = await loadAssets() // asyn wait +try/catch pour les consignes
+    const game = new game( { canvas, ctx, assets} );
+    game.start() // démarre la boucle du jeu, phase ""menu" tant que l'utilisateur n'a pas cliqué Play
+  } catch (e) {
+    // Erreur d'assets possible?
+    ctx.fillStyle = 'crimson';
+    ctx.font = '16px monostyle';
+    ctx.fillText('Erreur de chargement des images, vérif le dossier assets/ et les liens entre les fichiers', 20, 40);
+  }
+}
+bootstrap();
